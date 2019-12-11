5f98751b23c753b4b3cb55759c7250e577c886d7
gmanoli <gastonmanoli@gmail.com>
2019-12-11 14:30:34 -0300

    adding spring-cloud-config client support

--- src/main/java/com/mspoc/users/controller/UserController.java
+++ src/main/java/com/mspoc/users/controller/UserController.java
@@ -1,10 +1,14 @@
 package com.mspoc.users.controller;
 
 import com.mspoc.users.service.UserService;
+import lombok.extern.java.Log;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
+@Log
 public class UserController {
 
     private final UserService userService;
@@ -15,6 +19,7 @@ public class UserController {
 
     @GetMapping("/mock-user-test")
     public String getMockUser() {
+        log.info("retrieving mock user");
         return userService.showMock();
     }
 
--- src/main/java/com/mspoc/users/service/UserService.java
+++ src/main/java/com/mspoc/users/service/UserService.java
@@ -1,13 +1,17 @@
 package com.mspoc.users.service;
 
 import com.mspoc.users.model.User;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 
+    @Value("${my.test:Config Server is not working. Please check...}")
+    private String mockUserName;
+
     public String showMock() {
-        User user = new User(1L, "mock");
+        User user = new User(1L, mockUserName);
         return user.getName();
     }
 }
